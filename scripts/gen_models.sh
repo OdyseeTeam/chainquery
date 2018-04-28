2374fd38d81b529343bc3d78634b3bf29da1d6c3
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-04-28 10:47:44 -0400

    -moved low usage scripts to directory for better organization.

--- scripts/gen_models.sh
+++ scripts/gen_models.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (
   cd "$DIR"
   go get -u -t github.com/volatiletech/sqlboiler
-  sqlboiler --no-auto-timestamps --no-hooks --tinyint-as-bool --wipe mysql
+  sqlboiler --no-auto-timestamps --no-hooks --no-tests --tinyint-as-bool --wipe mysql
 )
