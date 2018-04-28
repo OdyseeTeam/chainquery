7ad6508c3de9fda3692b48c5d7f5f45c605fe11e
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-04-28 11:14:54 -0400

    -moved swagger generation items and reworked api gen script for move.

--- scripts/gen_apis.sh
+++ scripts/gen_apis.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 (
     #download swagger-codegen-cli if not already there.
-    cd "$DIR"
-    mkdir -p $DIR/swagger
-    if [ ! -f $DIR/swagger-codegen-cli.jar ]; then
+    cd "$PWD"
+    mkdir -p $PWD/swagger
+    if [ ! -f $PWD/swagger/swagger-codegen-cli.jar ]; then
         echo "swagger cli not found, downloading..."
-        wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O ./swagger-codegen-cli.jar
+        wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O ./swagger/swagger-codegen-cli.jar
     fi
     #Generate API docs
 
-    executable="./swagger-codegen-cli.jar"
+    executable="$PWD/swagger/swagger-codegen-cli.jar"
 
     export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties"
-    agsDocs="$@ generate -i $DIR/chainquery.yaml -l dynamic-html  -o $DIR"
-    agsServer="$@ generate -i $DIR/chainquery.yaml -l go-server -t $DIR/swagger/modules/go-server -Dmodel={}  -o swagger/apiserver"
-    agsClient_go="$@ generate -i $DIR/chainquery.yaml -l go  -o swagger/clients/goclient"
-    agsClient_python="$@ generate -i $DIR/chainquery.yaml -l python  -o swagger/clients/pythonclient"
+    agsDocs="$@ generate -i $PWD/swagger/chainquery.yaml -l dynamic-html  -o $PWD"
+    agsServer="$@ generate -i $PWD/swagger/chainquery.yaml -l go-server -t $PWD/swagger/modules/go-server -Dmodel={}  -o $PWD/swagger/apiserver"
+    agsClient_go="$@ generate -i $PWD/swagger/chainquery.yaml -l go  -o $PWD/swagger/clients/goclient"
+    agsClient_python="$@ generate -i $PWD/swagger/chainquery.yaml -l python  -o $PWD/swagger/clients/pythonclient"
 
 
     java $JAVA_OPTS -jar $executable $agsDocs
