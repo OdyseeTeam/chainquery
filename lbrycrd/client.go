65809680fcf199c11b9c7fa5a7ff9cce802b54d2
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-04-28 23:52:21 -0400

    -cleaned up large function with switch cases to abide by gocyclo rules

--- lbrycrd/client.go
+++ lbrycrd/client.go
@@ -96,6 +96,7 @@ func decode(data interface{}, targetStruct interface{}) error {
 	return nil
 }
 
+//ToDo - Can we just use the decodeInt function? Is it double for nothing?
 func decodeNumber(data interface{}) (decimal.Decimal, error) {
 	var number string
 	switch d := data.(type) {
@@ -118,81 +119,32 @@ func decodeNumber(data interface{}) (decimal.Decimal, error) {
 	return dec, nil
 }
 
+type protoFunc func(interface{}) (interface{}, error)
+
+type protoMap map[reflect.Type]protoFunc
+
+var decodeMap = protoMap{
+	reflect.TypeOf(uint64(0)):                         decodeInt,
+	reflect.TypeOf([]byte{}):                          decodeBytes,
+	reflect.TypeOf(decimal.Decimal{}):                 decodeFloat,
+	reflect.TypeOf(lbryschema.Metadata_Version(0)):    decodeMetaDataVersion,
+	reflect.TypeOf(lbryschema.Metadata_Language(0)):   decodeMetaDataLanguage,
+	reflect.TypeOf(lbryschema.Stream_Version(0)):      decodeStreamVersion,
+	reflect.TypeOf(lbryschema.Claim_Version(0)):       decodeClaimVersion,
+	reflect.TypeOf(lbryschema.Claim_ClaimType(0)):     decodeClaimType,
+	reflect.TypeOf(lbryschema.Fee_Version(0)):         decodeFeeVersion,
+	reflect.TypeOf(lbryschema.Fee_Currency(0)):        decodeFeeCurrency,
+	reflect.TypeOf(lbryschema.Source_Version(0)):      decodeSourceVersion,
+	reflect.TypeOf(lbryschema.Source_SourceTypes(0)):  decodeSourceTypes,
+	reflect.TypeOf(lbryschema.KeyType(0)):             decodeKeyType,
+	reflect.TypeOf(lbryschema.Signature_Version(0)):   decodeSignatureVersion,
+	reflect.TypeOf(lbryschema.Certificate_Version(0)): decodeCertificateVersion,
+}
+
 func fixDecodeProto(src, dest reflect.Type, data interface{}) (interface{}, error) {
-	switch dest {
-	case reflect.TypeOf(uint64(0)):
-		if n, ok := data.(json.Number); ok {
-			val, err := n.Int64()
-			if err != nil {
-				return nil, errors.Wrap(err, 0)
-			} else if val < 0 {
-				return nil, errors.Base("must be unsigned int")
-			}
-			return uint64(val), nil
-		}
-	case reflect.TypeOf([]byte{}):
-		if s, ok := data.(string); ok {
-			return []byte(s), nil
-		}
 
-	case reflect.TypeOf(decimal.Decimal{}):
-		if n, ok := data.(json.Number); ok {
-			val, err := n.Float64()
-			if err != nil {
-				return nil, errors.Wrap(err, 0)
-			}
-			return decimal.NewFromFloat(val), nil
-		} else if s, ok := data.(string); ok {
-			d, err := decimal.NewFromString(s)
-			if err != nil {
-				return nil, errors.Wrap(err, 0)
-			}
-			return d, nil
-		}
-
-	case reflect.TypeOf(lbryschema.Metadata_Version(0)):
-		val, err := getEnumVal(lbryschema.Metadata_Version_value, data)
-		return lbryschema.Metadata_Version(val), err
-	case reflect.TypeOf(lbryschema.Metadata_Language(0)):
-		val, err := getEnumVal(lbryschema.Metadata_Language_value, data)
-		return lbryschema.Metadata_Language(val), err
-
-	case reflect.TypeOf(lbryschema.Stream_Version(0)):
-		val, err := getEnumVal(lbryschema.Stream_Version_value, data)
-		return lbryschema.Stream_Version(val), err
-
-	case reflect.TypeOf(lbryschema.Claim_Version(0)):
-		val, err := getEnumVal(lbryschema.Claim_Version_value, data)
-		return lbryschema.Claim_Version(val), err
-	case reflect.TypeOf(lbryschema.Claim_ClaimType(0)):
-		val, err := getEnumVal(lbryschema.Claim_ClaimType_value, data)
-		return lbryschema.Claim_ClaimType(val), err
-
-	case reflect.TypeOf(lbryschema.Fee_Version(0)):
-		val, err := getEnumVal(lbryschema.Fee_Version_value, data)
-		return lbryschema.Fee_Version(val), err
-	case reflect.TypeOf(lbryschema.Fee_Currency(0)):
-		val, err := getEnumVal(lbryschema.Fee_Currency_value, data)
-		return lbryschema.Fee_Currency(val), err
-
-	case reflect.TypeOf(lbryschema.Source_Version(0)):
-		val, err := getEnumVal(lbryschema.Source_Version_value, data)
-		return lbryschema.Source_Version(val), err
-	case reflect.TypeOf(lbryschema.Source_SourceTypes(0)):
-		val, err := getEnumVal(lbryschema.Source_SourceTypes_value, data)
-		return lbryschema.Source_SourceTypes(val), err
-
-	case reflect.TypeOf(lbryschema.KeyType(0)):
-		val, err := getEnumVal(lbryschema.KeyType_value, data)
-		return lbryschema.KeyType(val), err
-
-	case reflect.TypeOf(lbryschema.Signature_Version(0)):
-		val, err := getEnumVal(lbryschema.Signature_Version_value, data)
-		return lbryschema.Signature_Version(val), err
-
-	case reflect.TypeOf(lbryschema.Certificate_Version(0)):
-		val, err := getEnumVal(lbryschema.Certificate_Version_value, data)
-		return lbryschema.Certificate_Version(val), err
+	if f, ok := decodeMap[dest]; ok {
+		return f(data)
 	}
 
 	return data, nil
@@ -208,4 +160,101 @@ func getEnumVal(enum map[string]int32, data interface{}) (int32, error) {
 		return 0, errors.Base("invalid enum key")
 	}
 	return val, nil
+}
+
+func decodeInt(data interface{}) (interface{}, error) {
+	if n, ok := data.(json.Number); ok {
+		val, err := n.Int64()
+		if err != nil {
+			return nil, errors.Wrap(err, 0)
+		} else if val < 0 {
+			return nil, errors.Base("must be unsigned int")
+		}
+		return uint64(val), nil
+	}
+	return data, nil
+}
+
+func decodeFloat(data interface{}) (interface{}, error) {
+	if n, ok := data.(json.Number); ok {
+		val, err := n.Float64()
+		if err != nil {
+			return nil, errors.Wrap(err, 0)
+		}
+		return decimal.NewFromFloat(val), nil
+	} else if s, ok := data.(string); ok {
+		d, err := decimal.NewFromString(s)
+		if err != nil {
+			return nil, errors.Wrap(err, 0)
+		}
+		return d, nil
+	}
+	return data, nil
+}
+
+func decodeBytes(data interface{}) (interface{}, error) {
+	if s, ok := data.(string); ok {
+		return []byte(s), nil
+	}
+	return data, nil
+}
+
+func decodeMetaDataVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Metadata_Version_value, data)
+	return lbryschema.Metadata_Version(val), err
+}
+
+func decodeMetaDataLanguage(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Metadata_Language_value, data)
+	return lbryschema.Metadata_Language(val), err
+}
+
+func decodeStreamVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Stream_Version_value, data)
+	return lbryschema.Stream_Version(val), err
+}
+
+func decodeClaimVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Claim_Version_value, data)
+	return lbryschema.Claim_Version(val), err
+}
+
+func decodeClaimType(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Claim_ClaimType_value, data)
+	return lbryschema.Claim_ClaimType(val), err
+}
+
+func decodeFeeVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Fee_Version_value, data)
+	return lbryschema.Fee_Version(val), err
+}
+
+func decodeFeeCurrency(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Fee_Currency_value, data)
+	return lbryschema.Fee_Currency(val), err
+}
+
+func decodeSourceVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Source_Version_value, data)
+	return lbryschema.Source_Version(val), err
+}
+
+func decodeSourceTypes(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Source_SourceTypes_value, data)
+	return lbryschema.Source_SourceTypes(val), err
+}
+
+func decodeKeyType(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.KeyType_value, data)
+	return lbryschema.KeyType(val), err
+}
+
+func decodeSignatureVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Signature_Version_value, data)
+	return lbryschema.Signature_Version(val), err
+}
+
+func decodeCertificateVersion(data interface{}) (interface{}, error) {
+	val, err := getEnumVal(lbryschema.Certificate_Version_value, data)
+	return lbryschema.Certificate_Version(val), err
 }
