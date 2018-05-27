fed5040d95449e1f63f1f9bf378f848336a0e483
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-05-26 23:34:27 -0400

    -Added indices to make spent claims more easily queryable.

--- migration/004_new_indices.sql
+++ migration/004_new_indices.sql
@@ -9,6 +9,14 @@ ALTER TABLE `claim` ADD INDEX `Idx_FeeAddress` (`fee_address` ASC);
 -- +migrate StatementEnd
 
 -- +migrate StatementBegin
+ALTER TABLE `output` ADD INDEX `Idx_SpentOutput` (`transaction_hash` ASC, `vout` ASC, `is_spent` ASC) COMMENT 'used for grabbing spent outputs with joins';
+-- +migrate StatementEnd
+
+-- +migrate StatementBegin
+ALTER TABLE `claim` ADD INDEX `Idx_ClaimOutpoint` (`transaction_by_hash_id` ASC, `vout` ASC) COMMENT 'used for match claim to output with joins';
+-- +migrate StatementEnd
+
+-- +migrate StatementBegin
 ALTER TABLE `claim` ADD COLUMN `claim_address` VARCHAR(40) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT '';
 -- +migrate StatementEnd
 
