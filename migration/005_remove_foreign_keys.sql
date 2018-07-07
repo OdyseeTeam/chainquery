f9fafeac4d0fe7142b443bae6d7cb7157e3c0cc3
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-07-07 00:35:25 -0400

    fixed bug removing the schema prefix which could cause issues when testing with another database and cause migration to fail.

--- migration/005_remove_foreign_keys.sql
+++ migration/005_remove_foreign_keys.sql
@@ -1,28 +1,28 @@
 -- +migrate Up
 
 -- +migrate StatementBegin
-ALTER TABLE chainquery.claim
+ALTER TABLE claim
   DROP FOREIGN KEY claim_ibfk_2;
 -- +migrate StatementEnd
 
 -- +migrate StatementBegin
-ALTER TABLE chainquery.input
+ALTER TABLE input
   DROP FOREIGN KEY input_ibfk_1;
 -- +migrate StatementEnd
 
 -- +migrate StatementBegin
-ALTER TABLE chainquery.support
+ALTER TABLE support
   DROP FOREIGN KEY support_ibfk_1,
   CHANGE COLUMN transaction_hash transaction_hash_id VARCHAR(70) CHARACTER SET 'latin1' COLLATE 'latin1_general_ci' NULL DEFAULT NULL,
   ADD CONSTRAINT fk_transaction
     FOREIGN KEY (transaction_hash_id)
-    REFERENCES chainquery.transaction (hash)
+    REFERENCES transaction (hash)
       ON DELETE CASCADE
       ON UPDATE NO ACTION;
 -- +migrate StatementEnd
 
 -- +migrate StatementBegin
-ALTER TABLE chainquery.output
+ALTER TABLE output
   DROP FOREIGN KEY output_ibfk_3,
   DROP FOREIGN KEY output_ibfk_2;
 -- +migrate StatementEnd
