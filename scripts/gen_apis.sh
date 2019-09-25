77010f7693519b0b8939b22bc7521640aaf67ff0
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-09-24 20:40:24 -0400

    Update api generation

--- scripts/gen_apis.sh
+++ scripts/gen_apis.sh
@@ -5,7 +5,7 @@
     mkdir -p $PWD/swagger
     if [ ! -f $PWD/swagger/swagger-codegen-cli.jar ]; then
         echo "swagger cli not found, downloading..."
-        wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O ./swagger/swagger-codegen-cli.jar
+        wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.8/swagger-codegen-cli-2.4.8.jar -O ./swagger/swagger-codegen-cli.jar
     fi
     #Generate API docs
 
