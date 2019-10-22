2ea85b3336ae0a6bd62f0bed869150516850ce2d
Niko Storni <niko@lbry.com>
2019-10-22 17:30:29 -0400

    fix columns

--- migration/015_adjust_column_sizes.sql
+++ migration/015_adjust_column_sizes.sql
@@ -2,7 +2,7 @@
 
 -- +migrate StatementBegin
 ALTER TABLE `tag`
-    CHANGE COLUMN `tag` `tag` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL;
+    CHANGE COLUMN `tag` `tag` VARCHAR(500) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL;
 -- +migrate StatementEnd
 
 -- +migrate StatementBegin
--- migration/016_transaction_hashes_adjustment.sql
+++ migration/016_transaction_hashes_adjustment.sql
@@ -4,3 +4,13 @@
 ALTER TABLE `block`
     CHANGE COLUMN `transaction_hashes` `transaction_hashes` MEDIUMTEXT;
 -- +migrate StatementEnd
+
+-- +migrate StatementBegin
+ALTER TABLE claim
+    DROP INDEX Idx_License;
+-- +migrate StatementEnd
+
+-- +migrate StatementBegin
+ALTER TABLE `claim`
+    CHANGE COLUMN `license` `license` TEXT;
+-- +migrate StatementEnd
