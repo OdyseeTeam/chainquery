1ad19626746324afb976dfe5c7b7e3685386e93a
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-11-01 20:44:31 -0400

    FIX nometadata argument removal

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go-bindata -nometadata -o migration/bindata.go -pkg migration -ignore bindata.go migration/
+//go:generate go-bindata -o migration/bindata.go -pkg migration -ignore bindata.go migration/
 //go:generate go fmt ./migration/bindata.go
 //go:generate goimports -l ./migration/bindata.go
 
