ec02e44509eebdefb90be861bc3ffcf4e48c68c5
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-12-08 14:52:18 -0500

    Fix certificate sync job so it doesn't use an expensive query.

--- daemon/jobs/certificatesync.go
+++ daemon/jobs/certificatesync.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/lbryio/chainquery/global"
 	"github.com/lbryio/chainquery/model"
+
 	"github.com/lbryio/lbry.go/extras/errors"
 	c "github.com/lbryio/lbryschema.go/claim"
 
 	"github.com/sirupsen/logrus"
+	"github.com/volatiletech/null"
 	"github.com/volatiletech/sqlboiler/boil"
 	"github.com/volatiletech/sqlboiler/queries"
 )
@@ -31,7 +33,11 @@ func CertificateSync() {
 			logrus.Error(certificateSyncPrefix+" Unable to get claims that need certificates checked", errors.Err(err))
 		}
 		for _, claimToBeSynced := range claims {
-
+			err := claimToBeSynced.populateFirstInputInfo()
+			if err != nil {
+				logrus.Error(certificateSyncPrefix+"", err)
+				continue
+			}
 			claim := model.Claim{ID: claimToBeSynced.ID}
 			certified, err := certifyClaim(claimToBeSynced)
 			if err != nil {
@@ -87,10 +93,24 @@ type claimToBeSynced struct {
 	ID                    uint64 `boil:"id"`
 	SignedClaimHex        string `boil:"signed_claim_hex"`
 	SignedClaimAddress    string `boil:"claim_address"`
+	TransactionHashUpdate string `boil:"transaction_hash_update"`
 	ChannelHex            string `boil:"channel_hex"`
 	ChannelClaimID        string `boil:"claim_id"`
 	FirstInputTxHash      string `boil:"first_input_tx_hash"`
 	FirstInputTxOPosition uint64 `boil:"first_input_txo_position"`
+}
+
+func (c *claimToBeSynced) populateFirstInputInfo() error {
+	w := model.InputWhere
+	input, err := model.Inputs(w.TransactionHash.EQ(c.TransactionHashUpdate), w.Vin.EQ(null.UintFrom(0))).OneG()
+	if err != nil {
+		return errors.Err(err)
+	}
+	if input != nil {
+		c.FirstInputTxHash = input.PrevoutHash.String
+		c.FirstInputTxOPosition = uint64(input.PrevoutN.Uint)
+	}
+	return nil
 }
 
 func getClaimsToBeSynced() ([]claimToBeSynced, error) {
@@ -99,6 +119,7 @@ func getClaimsToBeSynced() ([]claimToBeSynced, error) {
 	claimID := claim + "." + model.ClaimColumns.ID
 	signedClaimHex := claim + "." + model.ClaimColumns.ValueAsHex + " as signed_claim_hex"
 	claimAddress := claim + "." + model.ClaimColumns.ClaimAddress
+	claimTxHashUpdate := claim + "." + model.ClaimColumns.TransactionHashUpdate
 	channelHex := "channel." + model.ClaimColumns.ValueAsHex + " as channel_hex"
 	ChannelClaimID := "channel." + model.ClaimColumns.ClaimID
 	publisherID := claim + "." + model.ClaimColumns.PublisherID
@@ -111,12 +132,10 @@ func getClaimsToBeSynced() ([]claimToBeSynced, error) {
 			`+signedClaimHex+`,
 			`+claimAddress+`,
 			`+channelHex+`,
-			`+ChannelClaimID+`, 
-			(SELECT prevout_hash FROM input i WHERE i.id = input.id) as first_input_tx_hash,
-			(SELECT prevout_n FROM input i WHERE i.id = input.id) as first_input_txo_position
+			`+ChannelClaimID+`,
+			`+claimTxHashUpdate+`
 		FROM `+claim+`
 		INNER JOIN `+claim+` channel ON `+ChannelClaimID+` = `+publisherID+` 
-		INNER JOIN input ON input.transaction_hash = claim.transaction_hash_update AND input.vin = 0 
 		WHERE `+isCertProcessed+`=? LIMIT ?`, false, certsProcessedPerIteration).BindG(context, &claims)
 	if err != nil {
 		return nil, err
