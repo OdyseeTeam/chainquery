1b64ab87e70f63a8bb116e9aadd8bd6541d9f8fb
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-01-04 11:58:44 -0500

    avoid pull limits from docker

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ if [ $# -eq 0 ]
     echo "No docker tag argument supplied. Use './build.sh <tag>'"
     exit 1
 fi
-docker build --no-cache --build-arg VERSION=$1 --tag lbry/chainquery:$1 ./docker
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker build --no-cache --build-arg VERSION=$1 --tag lbry/chainquery:$1 ./docker
 docker push lbry/chainquery:$1
